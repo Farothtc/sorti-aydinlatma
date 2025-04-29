8f66538affe457cf2c363f2394ed23c06c9cf4de
Farothtc <mvkirimi@gmail.com>
2025-04-29 18:17:28 +0300

    Initial commit

---
+++ src/app/Components/Nav.tsx
@@ -0,0 +1,71 @@
+import Link from "next/link";
+export default function Nav() {
+  return (
+    <div className="navbar bg-transparent container mx-auto flex justify-evenly items-center pt-6">
+      <Link href="/">
+        <div className="flex flex-col">
+          <p className="text-5xl">Sorti</p>
+          <p className="">AYDINLATMA</p>
+        </div>
+      </Link>
+      <div className="flex flex-col items-center">
+        <label className="input bg-transparent border-white mx-auto flex items-center ms-24 rounded-3xl">
+          <input type="search" className="grow " />
+          <svg
+            className="h-[1em]"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+        </label>
+      </div>
+      <div className="">
+        <ul className="menu menu-horizontal px-1 text-2xl text-white">
+          <Link href="/">
+            <li>
+              <button>Anasayfa</button>
+            </li>
+          </Link>
+          <Link href="/urunler">
+            <li>
+              <button>Ürünler</button>
+            </li>
+          </Link>
+          <Link href="/">
+            <li>
+              <button>Hakkımızda</button>
+            </li>
+          </Link>
+          <Link href="/">
+            <li>
+              <button>Galeri</button>
+            </li>
+          </Link>
+          {/* <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="bg-transparent w-32 h-auto rounded-t-none p-2">
+                <li>
+                  <a>Link 1</a>
+                </li>
+                <li>
+                  <a>Link 2</a>
+                </li>
+              </ul>
+            </details>
+          </li> */}
+        </ul>
+      </div>
+    </div>
+  );
+}
